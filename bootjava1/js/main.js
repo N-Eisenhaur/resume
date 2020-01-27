d3840729557abd63a9b5583b4f43b53ed391b2ec
Nathan Eisenhaur <nathan@Nathans-MacBook-Pro.local>
2020-01-27 17:33:38 -0400

    bootjava v 1

---
+++ bootjava1/js/main.js
@@ -0,0 +1,28 @@
+$('.btn btn-outline-primary').on('click', function(){
+ 
+  //The new text that we want to show.
+  var newText = 'This text has been changed!';
+
+  //Change the text using the text method.
+  $('.jumbotron .jumbotron-fluid  .jumbopic').text(newText);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function showMessage() {  
+    alert( 'Hello ' );  
+    
+  }
+  
+  document.getElementById("btn btn-outline-primary").onclick=showMessage */
