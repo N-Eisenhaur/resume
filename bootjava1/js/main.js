50227ee7507c1f131276a3b79380ad2918f3e776
Nathan Eisenhaur <nathan@Nathans-MacBook-Pro.local>
2020-01-28 12:46:21 -0400

    bootjava v2

--- bootjava1/js/main.js
+++ bootjava1/js/main.js
@@ -1,10 +1,10 @@
-$('.btn btn-outline-primary').on('click', function(){
+$('#Ctext').on('click', function(){
  
   //The new text that we want to show.
   var newText = 'This text has been changed!';
 
   //Change the text using the text method.
-  $('.jumbotron .jumbotron-fluid  .jumbopic').text(newText);
+  $('#Mbox').text(newText);
 
 });
 
