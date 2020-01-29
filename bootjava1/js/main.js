1e48caee422482f8f5f2c064087476e5f0dfa3c8
Nathan Eisenhaur <nathan@Nathans-MBP.lan>
2020-01-29 13:57:39 -0400

    bootjava v3 finished

--- bootjava1/js/main.js
+++ bootjava1/js/main.js
@@ -1,10 +1,32 @@
-$('#Ctext').on('click', function(){
- 
+
+console.log("hi")
+
+$('#Ctext').on('click', function () {
+
   //The new text that we want to show.
-  var newText = 'This text has been changed!';
+  var newText = ['This text has been changed!',
+    'new text 2',
+    'really new text 3'
+
+
+
+  ];
+// console logs between each step can tell you what your doing
+
+var arraySize = newText.length // arraySize is finding length of newText
+
+var randomNumber = (Math.random()*arraySize )  // .random picks from 0 to 1 * multiplies newText.length which is now arraySize
+
+
+var indexed = Math.floor(randomNumber); // math.floor rounds down 
+console.log("indexed is"+indexed)
+
+var selectedValue = newText[indexed] ; // anything [] are numbers 
+
+
 
   //Change the text using the text method.
-  $('#Mbox').text(newText);
+  $('#Mbox').text(selectedValue);
 
 });
 
@@ -20,9 +42,9 @@ $('#Ctext').on('click', function(){
 
 
 /*
-function showMessage() {  
-    alert( 'Hello ' );  
-    
+function showMessage() {
+    alert( 'Hello ' );
+
   }
-  
+
   document.getElementById("btn btn-outline-primary").onclick=showMessage */
