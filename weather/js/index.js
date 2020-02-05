452c28c8d4e21c8f1fac951414ab11bf45ef2e46
Nathan Eisenhaur <nathan@Nathans-MBP.lan>
2020-02-05 14:28:07 -0400

    weather app 2

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,10 +1,67 @@
+var junk = {
+
+  base: "stations",
+  clouds: {
+    all: 1
+  },
+  coord: {
+    lat: 43.65,
+    lon: -79.38
+  },
+  dt: 1507510380,
+  id: 6167863,
+  main: {
+    humidity: 77,
+    pressure: 1014,
+    temp: 17.99,
+    temp_max: 20,
+    temp_min: 16
+  },
+  name: 'Downtown Toronto',
+  sys: {
+    type: 1,
+    id: 2117,
+    message: 0.0041,
+    country: 'CA',
+    sunrise: 1507548290,
+    sunset: 1507589027,
+    type: 1
+  },
+  visibility: 16093,
+  weather: [
+    {
+      description: 'clear sky',
+      icon: '01n',
+      id: 800,
+      main: "Clear"
+    }
+
+  ],
+  wind: {
+    deg: 170, speed: 1.5
+  }
+}
+
+
+
 $(document).ready(function () {
   // WRITE YOUR CODE HERE.
   console.log("hi")
 
+  $("#results").hide()
+
   $('form').on('submit', function (event) {
     event.preventDefault()
+    $("#results").html("<h3>Current Conditions For " + junk.name + "</h3>" + 
+    "<ul> <li>Temperature </li>" + "<li>"+ junk.main.temp + "</li>" +
+    " <li>conditions </li>" + "<li>" +junk.weather[0].description + "</li>" +"</ul>" )
+    $("#results").slideDown(2000)
 
+console.log($("#iField").val())
+
+$("#again").click(function () {
+  $("#results").slideUp(2000)
+});
   })
 
   $(function () {
@@ -31,4 +88,8 @@ $(document).ready(function () {
 
 
 
-  });
+
+
+
+
+});
