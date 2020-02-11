e058d01bef16fe68f7028cc2c3705fcf91aee424
Nathan Eisenhaur <nathan@Nathans-MBP.lan>
2020-02-11 13:55:26 -0400

    weather app 4? finished

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,49 +1,9 @@
- /* var api  = {"55d4f7e164b50a23056cf030f55ae99c"} */
+var api = "55d4f7e164b50a23056cf030f55ae99c";
 
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Toronto&appid=" + api
 
-var junk = {
+console.log(weatherUrl)
 
-  base: "stations",
-  clouds: {
-    all: 1
-  },
-  coord: {
-    lat: 43.65,
-    lon: -79.38
-  },
-  dt: 1507510380,
-  id: 6167863,
-  main: {
-    humidity: 77,
-    pressure: 1014,
-    temp: 17.99,
-    temp_max: 20,
-    temp_min: 16
-  },
-  name: 'Downtown Toronto',
-  sys: {
-    type: 1,
-    id: 2117,
-    message: 0.0041,
-    country: 'CA',
-    sunrise: 1507548290,
-    sunset: 1507589027,
-    type: 1
-  },
-  visibility: 16093,
-  weather: [
-    {
-      description: 'clear sky',
-      icon: '01n',
-      id: 800,
-      main: "Clear"
-    }
-
-  ],
-  wind: {
-    deg: 170, speed: 1.5
-  }
-}
 
 
 
@@ -53,18 +13,34 @@ $(document).ready(function () {
 
   $("#results").hide()
 
+
+
   $('form').on('submit', function (event) {
     event.preventDefault()
-    $("#results").html("<h3>Current Conditions For " + junk.name + "</h3>" + 
-    "<ul> <li>Temperature </li>" + "<li>"+ junk.main.temp + "</li>" +
-    " <li>conditions </li>" + "<li>" +junk.weather[0].description + "</li>" +"</ul>" )
-    $("#results").slideDown(2000)
 
-console.log($("#iField").val())
 
-$("#again").click(function () {
-  $("#results").slideUp(2000)
-});
+
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=" + $("#iField").val() + "&appid=" + api
+
+
+    $.get(weatherUrl, function (data) {
+      //console.log(data)
+
+      $("#results").html("<h3>Current Conditions For " + data.name + "</h3>" +
+        "<ul> <li>Temperature </li>" + "<li>" + data.main.temp + "</li>" +
+        " <li>conditions </li>" + "<li>" + data.weather[0].description + "</li>" + "</ul>")
+      $("#results").slideDown(2000)
+
+
+    })
+
+
+
+    console.log($("#iField").val())
+
+    $("#again").click(function () {
+      $("#results").slideUp(2000)
+    });
   })
 
   $(function () {
